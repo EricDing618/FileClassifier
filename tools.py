f7cfd8557d834a28e7f94b604b260408ccc0f87d
Eric学编程 <135501423+EricDing618@users.noreply.github.com>
2024-01-28 20:51:20 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,8 +1,20 @@
 from os import system
 from threading import Thread
+from PyQt5.QtWidgets import QMessageBox
+def about_bind():
+    if system('about.html'):
+        print('About File is open.')
+    else:
+        QMessageBox.critical(None,'错误','无法打开关于！')
+def help_bind():
+    if system('help.html'):
+        print('About File is open.')
+    else:
+        QMessageBox.critical(None,'错误','无法打开帮助！')
+
 def about():
-    a=Thread(target=system,args=('about.html',))
+    a=Thread(target=about_bind)
     a.start()
 def helps():
-    a=Thread(target=system,args=('help.html',))
+    a=Thread(target=help_bind)
     a.start()
