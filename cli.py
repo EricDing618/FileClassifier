9a2da12885805a28926e721c9035c8342b402585
Eric学编程 <135501423+EricDing618@users.noreply.github.com>
2024-02-13 21:22:50 +0800

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,11 @@
+"""
+以命令行的形式运行程序（源代码：main.py），可用于查看输出信息。
+在您运行或调试源代码文件，并且没有可执行文件时此文件无效。
+"""
+from os import system,getcwd
+import os.path
+if os.path.isfile(getcwd()+'\\classifier.exe'):
+    system('classifier.exe')
+else:
+    print('错误：可执行文件不存在！')
+input('按回车键退出本程序...')
